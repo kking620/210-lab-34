9af9d84cbc5d1518ba0cea87c8ad8cea08395466
Krispy King <kking620@gmail.com>
2025-11-22 18:24:50 -0800

    Third Commit

--- 210-demo-graph.cpp
+++ 210-demo-graph.cpp
@@ -9,42 +9,43 @@ using namespace std;
 
 const int SIZE = 7;
 
-struct Edge {
-    int src, dest, weight;
+struct BusRoute {
+    int src, dest, travelTime;
 };
 
-typedef pair<int, int> Pair;  // Creates alias 'Pair' for the pair<int,int> data type
+typedef pair<int, int> RouteLink;  // Creates alias 'Pair' for the pair<int,int> data type
 
-class Graph {
+class BusNetwork {
 public:
     // a vector of vectors of Pairs to represent an adjacency list
-    vector<vector<Pair>> adjList;
+    vector<vector<RouteLink>> adjList;
+    vector<string> stopNames;
 
     // Graph Constructor
-    Graph(vector<Edge> const &edges) {
+    BusNetwork(vector<BusRoute> const &routes, int numStops, vector<string> names) {
         // resize the vector to hold SIZE elements of type vector<Edge>
         adjList.resize(SIZE);
 
         // add edges to the directed graph
-        for (auto &edge: edges) {
-            int src = edge.src;
-            int dest = edge.dest;
-            int weight = edge.weight;
+        for (auto &route: routes) {
+            int src = route.src;
+            int dest = route.dest;
+            int travelTime = route.travelTime;
 
             // insert at the end
-            adjList[src].push_back(make_pair(dest, weight));
+            adjList[src].push_back(make_pair(dest, travelTime));
             // for an undirected graph, add an edge from dest to src also
-            adjList[dest].push_back(make_pair(src, weight));
+            adjList[dest].push_back(make_pair(src, travelTime));
         }
     }
 
     // Print the graph's adjacency list
     void printGraph() {
-        cout << "Graph's adjacency list:" << endl;
+        cout << "Bus Route Connections:" << endl;
         for (int i = 0; i < adjList.size(); i++) {
-            cout << i << " --> ";
-            for (Pair v : adjList[i])
-                cout << "(" << v.first << ", " << v.second << ") ";
+            cout << i << " connects to: ";
+            for (RouteLink v : adjList[i])
+                cout << " -> " << v.first << ") , (Number of Buses that Stop Here: " << v.second << ") ";
             cout << endl;
         }
     }
@@ -66,7 +67,7 @@ public:
         visited.insert(v);
         cout << v << " ";
 
-        for (Pair neighbor : adjList[v]) {
+        for (RouteLink neighbor : adjList[v]) {
             int nextVertex = neighbor.first;
             if (visited.find(nextVertex) == visited.end()) {
                 dfsUtil(nextVertex, visited);
@@ -92,7 +93,7 @@ public:
             q.pop_front();
             cout << current << " ";
 
-            for (Pair neighbor : adjList[current]) {
+            for (RouteLink neighbor : adjList[current]) {
                 int nextVertex = neighbor.first;
                 if (visited.find(nextVertex) == visited.end()) {
                     visited.insert(nextVertex);
@@ -107,14 +108,20 @@ public:
 };
 
 int main() {
+      vector<string> names = {
+        "Stop A (Downtown Terminal)", "Stop B (Central Station)", "Stop C (Uptown Hub)",
+        "Stop D (Market St)", "Stop E (City Hall)", "Stop F (Airport Terminal)",
+        "Stop G (Industrial Park)", "Stop H (Suburban Plaza)", "Stop I (Metro Center)"
+    };
+    
     // Creates a vector of graph edges/weights
-    vector<Edge> edges = {
+    vector<BusRoute> busRoute = {
         // (x, y, w) —> edge from x to y having weight w
         {0, 1, 8}, {0, 2, 21},{1, 2, 6}, {1, 3, 5}, {1, 4, 4},{2, 7, 11}, {2, 8, 8},{3, 4, 9},{5, 6, 10}, {5, 7, 15}, {5, 8, 5},{6, 7, 3}, {6, 8, 7}
     };
 
     // Creates graph
-    Graph graph(edges);
+    BusNetwork graph(busRoute, SIZE, names);
 
     // Prints adjacency list representation of graph
     graph.printGraph();
